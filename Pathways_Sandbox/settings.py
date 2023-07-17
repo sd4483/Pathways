5a69d49f94386d212130d639cf68a53a12160db0
sd4483 <sudheer.paturi@hotmail.com>
2023-07-17 16:33:43 +0100

    Setup sandbox environment for project

--- Pathways_Sandbox/settings.py
+++ Pathways_Sandbox/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "Pathways_Sandbox.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Pathways_Sandbox',
+        'USER': 'postgres',
+        'PASSWORD': 'symbian',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
