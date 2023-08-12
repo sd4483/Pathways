ea1ed11ad2dbc3102cd8a24a771aafb86d207d1e
sd4483 <sudheer.paturi@hotmail.com>
2023-08-12 19:53:22 +0530

    Pathways title and info changed to left side menu

--- pathways/models.py
+++ pathways/models.py
@@ -8,26 +8,31 @@ class Pathway(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="pathways")
     title = models.CharField(max_length=200)
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class ImageResource(models.Model):
     pathway = models.ForeignKey(Pathway, on_delete=models.CASCADE, related_name="image_resources")
     title = models.CharField(max_length=200)
     notes = models.TextField()
     image = models.ImageField(upload_to='resources/')
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class LinkResource(models.Model):
     pathway = models.ForeignKey(Pathway, on_delete=models.CASCADE, related_name="link_resources")
     title = models.CharField(max_length=200)
     notes = models.TextField()
     url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class TextResource(models.Model):
     pathway = models.ForeignKey(Pathway, on_delete=models.CASCADE, related_name="text_resources")
     title = models.CharField(max_length=200)
     content = HTMLField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class FileResource(models.Model):
     pathway = models.ForeignKey(Pathway, on_delete=models.CASCADE, related_name="file_resources")
     title = models.CharField(max_length=200)
     notes = models.TextField()
     attachment = models.FileField(upload_to='files/')
+    created_at = models.DateTimeField(auto_now_add=True)
