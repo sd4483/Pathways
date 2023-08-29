7a48a87c61b03fa6af8a447d6d260fb06a1977f8
sd4483 <sudheer.paturi@hotmail.com>
2023-08-29 18:44:15 +0530

    dj_database_url added

--- Pathways_Sandbox/settings.py
+++ Pathways_Sandbox/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +82,10 @@ WSGI_APPLICATION = "Pathways_Sandbox.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 DATABASES = {
     'default': {
