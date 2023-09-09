d5bd7221ae5f958ba2573fe0bcbea3e5ea58f71a
sd4483 <sudheer.paturi@hotmail.com>
2023-09-09 20:32:50 +0530

    removed tinymice again

--- pathways/models.py
+++ pathways/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tinymce.models import HTMLField
 from datetime import date, timedelta
 from django.db.models.signals import post_save
 from django.dispatch import receiver
